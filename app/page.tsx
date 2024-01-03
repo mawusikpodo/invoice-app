55cc803adf4e939b746b3add1e753b8da062c6dd
mawusikpodo <mawusikpodo@yahoo.com>
2024-01-03 18:45:58 +0000

    added README

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to SIA.</strong> This is a simple invoicing app{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              For Every company
             </a>
-            , brought to you by Vercel.
+            , brought to you by RMK.
           </p>
           <Link
             href="/login"
