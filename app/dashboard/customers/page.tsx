7be7e0fc4f6ce7730991ce37690fe7c85a32d82a
mawusikpodo <mawusikpodo@yahoo.com>
2024-01-08 13:51:17 +0000

    added invoices and authentication

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,19 @@
-const Customers = () => {
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+
+
+
+const Customers = async({
+    query,
+    currentPage,
+  }: {
+    query: string;
+    currentPage: number;
+  }) => {
+    const customers = await fetchFilteredCustomers(query);
+
     return ( <div>
-        Customers
+        <CustomersTable customers={customers}/>
     </div> );
 }
  
--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to SIA.</strong> This is a simple invoicing app{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              For Every company
+              Next.js Learn Course
             </a>
-            , brought to you by RMK.
+            , brought to you by Vercel.
           </p>
           <Link
             href="/login"
